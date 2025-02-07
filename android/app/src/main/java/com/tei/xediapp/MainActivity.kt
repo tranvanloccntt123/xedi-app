83d7fa766e51c3e889ee9da6428cd67805dda4c6
tiep.vv <tiep.vv@zinza.com.vn>
2025-02-07 17:07:06 +0700

    update logo

--- android/app/src/main/java/com/tei/xediapp/MainActivity.kt
+++ android/app/src/main/java/com/tei/xediapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.xediapp
+package com.tei.xediapp
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/tei/xediapp/MainApplication.kt
+++ android/app/src/main/java/com/tei/xediapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.xediapp
+package com.tei.xediapp
 
 import android.app.Application
 import android.content.res.Configuration

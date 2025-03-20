6a8d63ec9a98a68ca65121472dea9545ad1b320f
teiresource <teiresource.loctran@gmail.com>
2025-03-20 10:55:52 +0700

    update

---
+++ src/components/FixedRoute/FixedRouteMergeList.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { IFixedRoute, ITripRequest, IUser } from "@/src/types";
+import { useSelector } from "react-redux";
+import { RootState } from "@/src/store/store";
+import { xediSupabase } from "@/src/lib/supabase";
+import { VStack } from "@/src/components/ui/vstack";
+import { HStack } from "@/src/components/ui/hstack";
+import { Heading } from "@/src/components/ui/heading";
+import { useNavigation } from "expo-router";
+import TripRequestItem from "../TripRequest/TripRequestItem";
+
+const FixedRouteMergeList: React.FC<{
+  fixedRoute: IFixedRoute;
+  isRefreshing?: boolean;
+}> = ({ fixedRoute, isRefreshing }) => {
+  const navigation = useNavigation();
+  const focused = navigation.isFocused();
+  const user: IUser = useSelector((state: RootState) => state.auth.user);
+  const [listFixedRouteRequest, setFixedRouteRequest] = React.useState<
+    ITripRequest[]
+  >([]);
+
+  const loadData = async () => {
+    const { data } =
+      await xediSupabase.tables.tripRequest.selectByUserIdAfterId({
+        filter: [
+          {
+            filed: "fixed_route_id",
+            filter: "eq",
+            data: fixedRoute.id,
+          },
+        ],
+      });
+    setFixedRouteRequest(data as never);
+  };
+
+  React.useEffect(() => {
+    if (user?.id || isRefreshing || focused) {
+      loadData();
+    }
+  }, [user, isRefreshing, focused]);
+
+  return (
+    <VStack space="md">
+      <HStack>
+        <Heading className="flex-1">Ghép chuyến</Heading>
+      </HStack>
+      {listFixedRouteRequest.map((v) => (
+        <TripRequestItem tripRequest={v} disabled={true} />
+      ))}
+    </VStack>
+  );
+};
+
+export default FixedRouteMergeList;

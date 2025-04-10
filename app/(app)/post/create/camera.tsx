3e709ec407d86aa06cbeafa4165a0f3ea10a2989
teiresource <teiresource.loctran@gmail.com>
2025-04-10 10:36:30 +0700

    show media

---
+++ app/(app)/post/create/camera.tsx
@@ -0,0 +1,160 @@
+import ChevronLeftIcon from "@/src/components/icons/ChevronLeftIcon";
+import { Box } from "@/src/components/ui/box";
+import { Button } from "@/src/components/ui/button";
+import { Center } from "@/src/components/ui/center";
+import { CameraImageSize } from "@/src/constants";
+import AppColors from "@/src/constants/colors";
+import { router } from "expo-router";
+import React, { useRef } from "react";
+import {
+  Image,
+  StatusBar,
+  TouchableOpacity,
+  useWindowDimensions,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { scale, ScaledSheet } from "react-native-size-matters";
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+} from "react-native-vision-camera";
+import { Asset, getAssetsAsync, usePermissions } from "expo-media-library";
+import { HStack } from "@/src/components/ui/hstack";
+
+const ImageThumbnail: React.FC<object> = () => {
+  const [image, setImage] = React.useState<Asset>();
+  const [permissionResponse, requestPermission] = usePermissions();
+
+  React.useEffect(() => {
+    const getThumbnail = async () => {
+      if (permissionResponse?.status !== "granted") {
+        await requestPermission();
+      }
+    };
+
+    getThumbnail();
+  }, []);
+
+  React.useEffect(() => {
+    if (permissionResponse?.status === "granted") {
+      getAssetsAsync()
+        .then((page) => {
+          console.log(page.assets);
+          setImage(page.assets[0]);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  }, [permissionResponse]);
+
+  return (
+    <TouchableOpacity
+      onPress={() => router.replace("/post/create/image-selection")}
+    >
+      <Box>
+        {!!image && (
+          <Image source={{ uri: image.uri }} style={styles.thumbnail} />
+        )}
+      </Box>
+    </TouchableOpacity>
+  );
+};
+
+export default function PostCamera() {
+  const insets = useSafeAreaInsets();
+  const { width } = useWindowDimensions();
+  const device = useCameraDevice("back");
+  const camera = useRef<Camera>(null);
+
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  React.useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission]);
+
+  const takePicture = async () => {
+    try {
+      const photo = await camera.current.takePhoto({});
+      console.log(photo.path);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <Box className="flex-1 bg-black">
+      <StatusBar translucent backgroundColor={"transparent"} />
+      <Box
+        style={{
+          width,
+          height: width * (CameraImageSize.height / CameraImageSize.width),
+          overflow: "hidden",
+          borderBottomLeftRadius: 25,
+          borderBottomRightRadius: 25,
+        }}
+      >
+        {router.canGoBack() && (
+          <Button
+            action="default"
+            className="absolute left-[16px] bg-xedi-background/[0.8] rounded-full p-0 justify-center items-center"
+            style={{
+              top: insets.top,
+              zIndex: 2,
+              width: scale(24),
+              height: scale(24),
+            }}
+            onPress={router.back}
+          >
+            <ChevronLeftIcon size={scale(18)} color={AppColors.text} />
+          </Button>
+        )}
+        {!!device && (
+          <Camera
+            ref={camera}
+            style={{
+              flex: 1,
+            }}
+            device={device}
+            isActive={true}
+            photo={true}
+          />
+        )}
+      </Box>
+      <Center
+        className="w-full absolute left-0 right-0"
+        style={{ bottom: insets.bottom + scale(16) }}
+      >
+        <HStack
+          className="w-full justify-between items-center"
+          style={{ paddingRight: scale(56), paddingLeft: scale(16) }}
+        >
+          <ImageThumbnail />
+          <Center className="flex-1">
+            <Button
+              onPress={takePicture}
+              className="h-[70px] w-[70px] rounded-full"
+              variant="outline"
+            >
+              <Box className="h-[50px] w-[50px] bg-white rounded-full" />
+            </Button>
+          </Center>
+        </HStack>
+      </Center>
+    </Box>
+  );
+}
+
+const styles = ScaledSheet.create({
+  thumbnail: {
+    width: "40@s",
+    height: "40@s",
+    resizeMode: "contain",
+    borderRadius: "5@s",
+    borderWidth: 1,
+    borderColor: "white",
+  },
+});
--- app/(app)/post/create/image-selection.tsx
+++ app/(app)/post/create/image-selection.tsx
@@ -1,28 +1,48 @@
-import ChevronLeftIcon from "@/src/components/icons/ChevronLeftIcon";
 import { Box } from "@/src/components/ui/box";
-import { Button } from "@/src/components/ui/button";
-import { Center } from "@/src/components/ui/center";
-import { CameraImageSize } from "@/src/constants";
-import AppColors from "@/src/constants/colors";
-import { router } from "expo-router";
-import React, { useRef } from "react";
-import { StatusBar, useWindowDimensions } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { scale } from "react-native-size-matters";
+import React from "react";
 import {
-  Camera,
-  useCameraDevice,
-  useCameraPermission,
-} from "react-native-vision-camera";
+  Image,
+  StatusBar,
+  TouchableOpacity,
+  useWindowDimensions,
+} from "react-native";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { scale, ScaledSheet } from "react-native-size-matters";
 import { Asset, getAssetsAsync, usePermissions } from "expo-media-library";
+import Animated, {
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import AppStyles from "@/src/theme/AppStyles";
+import AppColors from "@/src/constants/colors";
+import Header from "@/src/components/Header";
+import { HStack } from "@/src/components/ui/hstack";
+import { Button } from "@/src/components/ui/button";
+import CameraShutterIcon from "@/src/components/icons/CameraShutterIcon";
+import { router } from "expo-router";
 
-const ImageThumbnail: React.FC<object> = () => {
-  const [image, setImage] = React.useState<Asset>();
+const NUMS = 3;
+
+export default function ImageSelection() {
+  const { width } = useWindowDimensions();
+  const insets = useSafeAreaInsets();
+  const itemWidth = React.useMemo(() => width / NUMS - 0.5, [width]);
+  const [images, setImages] = React.useState<Asset[]>([]);
+  const [imagePreview, setImagePreview] = React.useState<Asset>();
   const [permissionResponse, requestPermission] = usePermissions();
+  const endCursor = React.useRef<string>();
+  const hasNextPage = React.useRef<boolean>(true);
+  const isLoading = React.useRef<boolean>(false);
+  const offsetY = useSharedValue(0);
 
   React.useEffect(() => {
     const getThumbnail = async () => {
-      if (permissionResponse.status !== "granted") {
+      if (permissionResponse?.status !== "granted") {
         await requestPermission();
       }
     };
@@ -30,96 +50,137 @@ const ImageThumbnail: React.FC<object> = () => {
     getThumbnail();
   }, []);
 
-  React.useEffect(() => {
-    if (permissionResponse.status === "granted") {
-      getAssetsAsync()
-        .then((page) => {
-          console.log(page.assets);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+  const fetchAssets = async () => {
+    if (isLoading.current || !hasNextPage.current) {
+      return;
     }
-  }, [permissionResponse])
-
-  return <Box></Box>;
-};
-
-export default function ImageSelection() {
-  const insets = useSafeAreaInsets();
-  const { width } = useWindowDimensions();
-  const device = useCameraDevice("back");
-  const camera = useRef<Camera>(null);
-
-  const { hasPermission, requestPermission } = useCameraPermission();
-
-  React.useEffect(() => {
-    if (!hasPermission) {
-      requestPermission();
-    }
-  }, [hasPermission]);
-
-  const takePicture = async () => {
-    try {
-      const photo = await camera.current.takePhoto({});
-      console.log(photo.path);
-    } catch (e) {
-      console.log(e);
-    }
+    isLoading.current = true;
+    getAssetsAsync(endCursor.current ? { after: endCursor.current } : undefined)
+      .then((page) => {
+        endCursor.current = page.endCursor;
+        hasNextPage.current = page.hasNextPage;
+        setImages([...images, ...page.assets]);
+        isLoading.current = false;
+      })
+      .catch(() => {
+        isLoading.current = false;
+      });
   };
 
-  return (
-    <Box className="flex-1 bg-black">
-      <StatusBar translucent backgroundColor={"transparent"} />
-      <Box
-        style={{
-          width,
-          height: width * (CameraImageSize.height / CameraImageSize.width),
-          overflow: "hidden",
-          borderBottomLeftRadius: 25,
-          borderBottomRightRadius: 25,
-        }}
-      >
-        {router.canGoBack() && (
-          <Button
-            action="default"
-            className="absolute left-[16px] bg-xedi-background/[0.8] rounded-full p-0 justify-center items-center"
-            style={{
-              top: insets.top,
-              zIndex: 2,
-              width: scale(24),
-              height: scale(24),
-            }}
-            onPress={router.back}
-          >
-            <ChevronLeftIcon size={scale(18)} color={AppColors.text} />
-          </Button>
-        )}
-        {!!device && (
-          <Camera
-            ref={camera}
-            style={{
-              flex: 1,
-            }}
-            device={device}
-            isActive={true}
-            photo={true}
-          />
-        )}
-      </Box>
-      <ImageThumbnail />
-      <Center
-        className="w-full absolute left-0 right-0"
-        style={{ bottom: insets.bottom + scale(16) }}
-      >
-        <Button
-          onPress={takePicture}
-          className="h-[70px] w-[70px] rounded-full"
-          variant="outline"
+  React.useEffect(() => {
+    if (permissionResponse?.status === "granted") {
+      fetchAssets();
+    }
+  }, [permissionResponse]);
+
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    offsetY.value = event.contentOffset.y;
+  });
+
+  const previewStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: -offsetY.value,
+        },
+      ],
+    };
+  });
+
+  const onSelectImage = (image: Asset) => {
+    offsetY.value = withTiming(0, { duration: 200 });
+    setImagePreview(image);
+  };
+
+  const renderItem = React.useCallback(
+    ({ item, index }: { item: Asset; index: number }) => (
+      <Box style={{ width: itemWidth, height: itemWidth, padding: scale(1) }}>
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          activeOpacity={0.8}
+          onPress={() => onSelectImage(item)}
         >
-          <Box className="h-[50px] w-[50px] bg-white rounded-full" />
-        </Button>
-      </Center>
+          <Image source={{ uri: item.uri }} style={styles.thumbnail} />
+        </TouchableOpacity>
+      </Box>
+    ),
+    []
+  );
+
+  return (
+    <Box className="flex-1 bg-xedi-background">
+      <StatusBar translucent={false} backgroundColor={"transparent"} />
+      <SafeAreaView style={AppStyles.container}>
+        <Box
+          style={[styles.navHeader, { paddingTop: insets.top }]}
+          className="px-[16px]"
+        >
+          <Header title="Chọn ảnh" />
+        </Box>
+        <Animated.View
+          style={[styles.previewContainer, { top: insets.top }, previewStyle]}
+        >
+          {!!imagePreview && (
+            <Image
+              source={{ uri: imagePreview.uri }}
+              style={{ flex: 1, resizeMode: "contain" }}
+            />
+          )}
+        </Animated.View>
+        <Animated.FlatList
+          bounces={false}
+          data={images}
+          keyExtractor={(item, index) => index.toString()}
+          ListHeaderComponent={
+            <Box>
+              <Box style={styles.listHeader} />
+              <HStack className="items-center justify-between">
+                <Button
+                  onPress={() => router.replace("/post/create/camera")}
+                  style={styles.cameraBtn}
+                  action="default"
+                >
+                  <CameraShutterIcon color={AppColors.text} size={scale(18)} />
+                </Button>
+              </HStack>
+            </Box>
+          }
+          renderItem={renderItem}
+          numColumns={NUMS}
+          onScroll={scrollHandler}
+          onEndReached={fetchAssets}
+          showsVerticalScrollIndicator={false}
+        />
+      </SafeAreaView>
     </Box>
   );
 }
+
+const styles = ScaledSheet.create({
+  thumbnail: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  listHeader: {
+    height: "400@vs",
+  },
+  navHeader: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    zIndex: 3,
+    top: 0,
+    backgroundColor: AppColors.background,
+  },
+  previewContainer: {
+    width: "100%",
+    height: "400@vs",
+    position: "absolute",
+    zIndex: 2,
+    backgroundColor: AppColors.background,
+  },
+  cameraBtn: {
+    width: "30@s",
+    height: "30@s",
+  },
+});
